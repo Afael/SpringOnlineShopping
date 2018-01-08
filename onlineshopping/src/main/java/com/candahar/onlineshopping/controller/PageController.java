d580c7123b174a8ed87b65662401e5e20d7c1201
Afael <jajang.developer@gmail.com>
2018-01-08 16:48:14 +0700

    Completed the interface theme and another issues

--- onlineshopping/src/main/java/com/candahar/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/candahar/onlineshopping/controller/PageController.java
@@ -6,31 +6,28 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
+	
 	@RequestMapping(value = {"/", "/index", "/home"})
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("index");//name of jsp file
-		mv.addObject("greeting", "Welcome to spring web mvc");
+		mv.addObject("title", "Home");
 		mv.addObject("userClickHome", true);		
 		return mv;
 	}
 	
-//	@RequestMapping(value="/test")
-//	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting){
-//		if(greeting == null){
-//			greeting = "Hello there";
-//		}
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("greeting", greeting);
-//		return mv;
-//	}
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact(){
+		ModelAndView mv = new ModelAndView("index");//name of jsp file
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);		
+		return mv;
+	}
 	
-//	@RequestMapping(value="/test/{greeting}")
-//	public ModelAndView test(@PathVariable("greeting") String greeting){
-//		if(greeting == null){
-//			greeting = "Hello there";
-//		}
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("greeting", greeting);
-//		return mv;
-//	}
+	@RequestMapping(value = "/about")
+	public ModelAndView about(){
+		ModelAndView mv = new ModelAndView("index");//name of jsp file
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);		
+		return mv;
+	}
 }
--- onlineshopping/src/main/webapp/WEB-INF/views/shared/navbar.jsp
+++ onlineshopping/src/main/webapp/WEB-INF/views/shared/navbar.jsp
@@ -14,9 +14,9 @@
 		<div class="collapse navbar-collapse"
 			id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
-				<li><a href="${contextRoot}/about">About</a></li>
-				<li><a href="#">Services</a></li>
-				<li><a href="#">Contact</a></li>
+				<li id="about"><a href="${contextRoot}/about">About</a></li>
+				<li id="listProducts"><a href="${contextRoot}/listProducts">View Product</a></li>
+				<li id="contact"><a href="${contextRoot}/contact">Contact</a></li>
 			</ul>
 		</div>
 		<!-- /.navbar-collapse -->
